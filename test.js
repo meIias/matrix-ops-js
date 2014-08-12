b4d2001139a0e0286e2c624989aaedeb71ef75fe
Moe Elias <moeelias@gmail.com>
2014-08-11 23:05:09 -0700

    Added  test data

--- test.js
+++ test.js
@@ -1,9 +1,36 @@
 
-
+// normal matrix
 var m1 = mtrx.create([
 
-	[1,2,3],
-	[4,5,6],
-	[7,8,9]
+	[1,2,3,6],
+	[4,5,6,7],
+	[7,3,4,11]
+	
+]);
+
+// diagonal matrix
+var m2 = mtrx.create([
+
+	[1,0,0],
+	[0,5,0],
+	[0,0,9]
+	
+]);
+
+// scalar matrix
+var m3 = mtrx.create([
+
+	[5,0,0],
+	[0,5,0],
+	[0,0,5]
+	
+]);
+
+// zero matrix
+var m4 = mtrx.create([
+
+	[0,0,0],
+	[0,0,0],
+	[0,0,0]
 	
 ]);
